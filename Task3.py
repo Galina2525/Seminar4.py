c3dddb9011e4dad6f1f05ab87b3ab8deaa580bc6
Galina <koroleva2525.gk@gmail.com>
2023-01-05 10:26:00 +0100

    changed  task

--- Task3.py
+++ Task3.py
@@ -6,10 +6,10 @@ def nok(x,y):
         smalller = y
     else:
         smalller = x
-    for i in range(1,smalller + 1):
+    for i in range(1,smalller + 1): # находим наибольший общий делитель
         if x % i == 0 and y % i == 0:
-            min_k = i
-    result = x * y / min_k        
+            max_k = i
+    result = x * y / max_k        # находим НОК по формуле
     return result    
 n1 = int(input('Введите первое число: '))    
 n2 = int(input('Введите второе число:')) 
